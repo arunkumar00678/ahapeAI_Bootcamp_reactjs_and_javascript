8e78e5a3915b3c975ab5ce02df756788a368df90
arunkumar00678 <74570319+arunkumar00678@users.noreply.github.com>
2021-06-27 16:56:10 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>JAVASCRIPT AND REACTJS</h1>
+      <p>The Foundation of Web Development everything from scratch including Javascript, React.js,HTML.</p>
+    </div>
+  );
+}
+export default Info;
